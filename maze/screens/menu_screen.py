f6f3e0c714f2b9528b4d4914f278ae5dfbf1ad71
Irene <irene9508@msn.com>
2020-10-23 23:10:54 +0200

    Added the init files for the maze and screens folder

--- maze/screens/menu_screen.py
+++ maze/screens/menu_screen.py
@@ -1,10 +1,17 @@
+from maze.screens.screen import Screen
 import pygame
 
-from maze.screens.screen import Screen
 
 class MenuScreen(Screen):
     def __init__(self, app):
         super().__init__(app)
+        self.background_image = None
+
+    def run(self):
+        self.background_image = pygame.image.load("background1")
+
+    def process_event(self, event):
+        pass
 
     def update(self):
         print("menu screen update")
