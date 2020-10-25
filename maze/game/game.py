7fc92974935fc69122703b7d97ce257be17d65fe
Irene <irene9508@msn.com>
2020-10-25 22:59:33 +0100

    added character and WASD support. Now working on bullet_entity

---
+++ maze/game/game.py
@@ -0,0 +1,30 @@
+class Game:
+    def __init__(self):
+        self.entities = []
+        self.entity_queue = []
+
+    def update(self):
+        for entity in self.entities:
+            entity.update()
+        self.initialize_entities()
+
+    def render(self, surface):
+        for entity in self.entities:
+            entity.render(surface)
+
+    def add_entity(self, entity):
+        self.entity_queue.append(entity)
+
+    def initialize_entities(self):
+        # remove dead entities:
+        new_entities = []
+        for entity in self.entities:
+            if entity.marked_for_destroy:
+                entity.destroy()
+            else:
+                new_entities.append(entity)
+        self.entities = new_entities
+
+        # add queued entities:
+        self.entities.extend(self.entity_queue)
+        self.entity_queue.clear()
