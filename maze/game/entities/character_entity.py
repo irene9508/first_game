13678f452fcf6fe505bf7a4657b61216a14d00c7
Irene <irene9508@msn.com>
2020-11-03 14:06:33 +0100

    updated alien animation + images, updated bullet shooting (now more time in between shots)

--- maze/game/entities/character_entity.py
+++ maze/game/entities/character_entity.py
@@ -3,93 +3,88 @@ from maze.game.entities.bullet_entity import BulletEntity
 import pygame
 from pygame import mixer
 
+
 class CharacterEntity(Entity):
     def __init__(self, game):
         super().__init__(game)
-        self.character1 = pygame.image.load("data/images/character1.png")
-        self.character2 = pygame.image.load("data/images/character2.png")
-        self.character3 = pygame.image.load("data/images/character3.png")
-        self.character4 = pygame.image.load("data/images/character4.png")
-        self.counter = 0
+        self.characters = [pygame.image.load("data/images/character1.png"),
+                           pygame.image.load("data/images/character2.png"),
+                           pygame.image.load("data/images/character3.png"),
+                           pygame.image.load("data/images/character4.png")]
+        self.animation_length = 0.1
+        self.characters_index = 0
+        self.shooting_timer = 0.2
         self.x = 280
         self.y = 300
 
     def update(self, delta_time):
+        self.animation_length -= delta_time
+        if self.animation_length <= 0:
+            self.characters_index += 1
+            if self.characters_index == 4:
+                self.characters_index = 0
+            self.animation_length = 0.1
+
         keys = pygame.key.get_pressed()
-        speed = 400
+        character_speed = 300
+        shot_speed = 0.2
 
         # moving:
         if keys[pygame.K_w] and not keys[pygame.K_s]:
-            self.y -= speed * delta_time
+            self.y -= character_speed * delta_time
             self.rotation = 0
         if keys[pygame.K_a] and not keys[pygame.K_d]:
-            self.x -= speed * delta_time
+            self.x -= character_speed * delta_time
             self.rotation = 90
         if keys[pygame.K_s] and not keys[pygame.K_w]:
-            self.y += speed * delta_time
+            self.y += character_speed * delta_time
             self.rotation = 180
         if keys[pygame.K_d] and not keys[pygame.K_a]:
-            self.x += speed * delta_time
+            self.x += character_speed * delta_time
             self.rotation = 270
 
         # shooting:
+        self.shooting_timer -= delta_time
         if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
             self.rotation = 0
-            pygame.mixer.stop()
-            bullet_sound = mixer.Sound('data/sounds/laser.wav')
-            bullet_sound.play()
-            self.game.add_entity(BulletEntity(self.game, self.x, self.y - 100,
-                                              self.rotation))
-        elif keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+            if self.shooting_timer <= 0:
+                pygame.mixer.stop()
+                bullet_sound = mixer.Sound('data/sounds/laser.wav')
+                bullet_sound.play()
+                self.game.add_entity(BulletEntity(self.game, self.x,
+                                                  self.y - 100, self.rotation))
+                self.shooting_timer = shot_speed
+        if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
             self.rotation = 90
-            pygame.mixer.stop()
-            bullet_sound = mixer.Sound('data/sounds/laser.wav')
-            bullet_sound.play()
-            self.game.add_entity(BulletEntity(self.game, self.x - 100, self.y,
-                                              self.rotation))
-        elif keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
+            if self.shooting_timer <= 0:
+                pygame.mixer.stop()
+                bullet_sound = mixer.Sound('data/sounds/laser.wav')
+                bullet_sound.play()
+                self.game.add_entity(BulletEntity(self.game, self.x - 100,
+                                                  self.y, self.rotation))
+                self.shooting_timer = shot_speed
+        if keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
             self.rotation = 180
-            pygame.mixer.stop()
-            bullet_sound = mixer.Sound('data/sounds/laser.wav')
-            bullet_sound.play()
-            self.game.add_entity(BulletEntity(self.game, self.x, self.y + 100,
-                                              self.rotation))
-        elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
+            if self.shooting_timer <= 0:
+                pygame.mixer.stop()
+                bullet_sound = mixer.Sound('data/sounds/laser.wav')
+                bullet_sound.play()
+                self.game.add_entity(BulletEntity(self.game, self.x,
+                                                  self.y + 100, self.rotation))
+                self.shooting_timer = shot_speed
+        if keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
             self.rotation = 270
-            pygame.mixer.stop()
-            bullet_sound = mixer.Sound('data/sounds/laser.wav')
-            bullet_sound.play()
-            self.game.add_entity(BulletEntity(self.game, self.x + 100, self.y,
-                                              self.rotation))
+            if self.shooting_timer <= 0:
+                pygame.mixer.stop()
+                bullet_sound = mixer.Sound('data/sounds/laser.wav')
+                bullet_sound.play()
+                self.game.add_entity(BulletEntity(self.game, self.x + 100,
+                                                  self.y, self.rotation))
+                self.shooting_timer = shot_speed
 
     def render(self, surface):
-        if self.counter < 20:
-            self.counter += 1
-            character = pygame.transform.rotate(self.character1, self.rotation)
-            width = character.get_size()[0]
-            height = character.get_size()[1]
-            surface.blit(character, (int(self.x - width / 2),
-                                     int(self.y - height / 2)))
-        elif self.counter < 40:
-            self.counter += 1
-            character = pygame.transform.rotate(self.character2, self.rotation)
-            width = character.get_size()[0]
-            height = character.get_size()[1]
-            surface.blit(character, (int(self.x - width / 2),
-                                     int(self.y - height / 2)))
-        elif self.counter < 60:
-            self.counter += 1
-            character = pygame.transform.rotate(self.character3, self.rotation)
-            width = character.get_size()[0]
-            height = character.get_size()[1]
-            surface.blit(character, (int(self.x - width / 2),
-                                     int(self.y - height / 2)))
-        elif self.counter >= 60:
-            self.counter = 0
-            character = pygame.transform.rotate(self.character4, self.rotation)
-            width = character.get_size()[0]
-            height = character.get_size()[1]
-            surface.blit(character, (int(self.x - width / 2),
-                                     int(self.y - height / 2)))
-
-# pygame.draw.rect(surface, (255, 255, 255), (self.x, self.y, 2, 2))
+        char = pygame.transform.rotate(self.characters[self.characters_index],
+                                       self.rotation)
+        width, height = char.get_size()[0], char.get_size()[1]
+        surface.blit(char, (int(self.x - width / 2),
+                            int(self.y - height / 2)))
